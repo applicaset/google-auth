52f8dab6c7fdac8aa825fc2cfbc8269aaccc3e7a
Naser Mirzaei <nasermirzaei89@gmail.com>
2020-12-14 22:00:36 +0330

    init
    Signed-off-by: Naser Mirzaei <nasermirzaei89@gmail.com>

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/applicaset/google-auth
+
+go 1.15
+
+require (
+	github.com/applicaset/user-svc v0.0.0-20201214180851-45ce42600b3e
+	github.com/pkg/errors v0.9.1
+)
---
+++ google_auth.go
@@ -0,0 +1,79 @@
+package google_auth
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"github.com/applicaset/user-svc"
+	"github.com/pkg/errors"
+	"net/http"
+)
+
+type googleAuth struct {
+	clientID string
+}
+
+type response struct {
+	id string
+}
+
+func (rsp response) Validated() bool {
+	return rsp.id != ""
+}
+
+func (rsp response) ID() string {
+	return rsp.id
+}
+
+type claims struct {
+	Subject  string `json:"sub"`
+	Audience string `json:"aud"`
+}
+
+func (ga *googleAuth) Validate(ctx context.Context, args map[string]interface{}) (user.ValidateResponse, error) {
+	rsp := new(response)
+	idToken, ok := args["id_token"]
+	if !ok {
+		return rsp, nil
+	}
+
+	u := fmt.Sprintf("https://oauth2.googleapis.com/tokeninfo?id_token=%s", idToken)
+
+	req, err := http.NewRequestWithContext(ctx, http.MethodPost, u, nil)
+	if err != nil {
+		return nil, errors.Wrap(err, "error on create new http request")
+	}
+
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return nil, errors.Wrap(err, "error on http get request to google")
+	}
+
+	defer func() { _ = res.Body.Close() }()
+
+	if res.StatusCode != http.StatusOK {
+		return rsp, nil
+	}
+
+	var c claims
+	err = json.NewDecoder(res.Body).Decode(&c)
+	if err != nil {
+		return nil, errors.Wrap(err, "error on decode response body")
+	}
+
+	if c.Audience != ga.clientID {
+		return rsp, nil
+	}
+
+	rsp.id = c.Subject
+
+	return rsp, nil
+}
+
+func NewAuthProvider(clientID string) user.AuthProvider {
+	ga := googleAuth{
+		clientID: clientID,
+	}
+
+	return &ga
+}
